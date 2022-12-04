e7561e417d4a990279985c05f21a44e4b3b9da34
Fengyuan Wu <wfy0425@yeah.net>
2022-12-03 22:32:04 -0800

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -3,3 +3,26 @@ module Main
   ) where
 
 import View (main)
+
+
+
+-- TODO:
+
+-- Tank
+-- - make move functionality work
+-- - direction
+-- - Add health
+
+-- - Add bullets
+
+-- gameboard
+-- - Add walls
+-- - Add score
+-- - Add game over
+-- - Add game win
+-- - Add game start
+-- - Add game pause
+-- - Add game restart
+-- - Add game quit
+-- - Add game menu
+-- - UI
